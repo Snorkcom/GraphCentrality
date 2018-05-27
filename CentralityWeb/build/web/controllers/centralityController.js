eb5be2a4f7b1c5b5857aaa88330dae0279571c6d
Snorkcom <Herobrine@yandex.ru>
2018-05-28 04:17:38 +0600

    Ранги бета

--- CentralityWeb/build/web/controllers/centralityController.js
+++ CentralityWeb/build/web/controllers/centralityController.js
@@ -8,11 +8,28 @@ class centralityController{
     
     calculateCentrality(number){
         
-         var data = JSON.parse(this.getData(number));
-         
+        $('rect').attr('width', 24 / 2)
+                    .attr('height', 24 / 2)
+                    .attr('fill', 'red')
+                    .attr('stroke', "None");
+                    
+        
+         var data = JSON.parse(this.getData(number));         
          console.log(data[1]);
          
-        $('#1').attr('fill', 'blue');
+         var x;
+         for(let i = 0; i<3;i++)
+         {
+             x =data[i]; 
+             alert(x[1]);
+             $('#'+x[1]).attr('fill', 'blue')
+                        .attr('width', 24)
+                        .attr('height', 24)
+                        .attr('stroke', "yellowgreen")
+                        .attr('stroke-width', '0.3%');                        
+         }
+         
+        
         
         
         var algorithm = number;
